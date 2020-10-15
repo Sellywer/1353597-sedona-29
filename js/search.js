5726a77a4e310da5a03babbd3aa435205a0b2b19
Sellywer <Sellywer@users.noreply.github.com>
2020-10-15 22:39:04 +0500

    кнопка показать

--- js/search.js
+++ js/search.js
@@ -1,7 +1,6 @@
 const searchField = document.querySelector('.search-field');
 const searchSedona = document.querySelector(".button-seach-sedona");
 const dateOn = searchField.querySelector("[name=date-on]");
-const dateOff = searchField.querySelector("[name=date-off]");
 const amountPeople = searchField.querySelector("[name=amount-of-people]");
 const amountChild = searchField.querySelector("[name=amount-of-child]");
 const form = searchField.querySelector("form"); 
