ea0c5afb07e2d8f077ba751dcfe682fa26475059
Sellywer <Sellywer@users.noreply.github.com>
2020-10-21 00:54:48 +0500

    Доработка главной страницы

--- js/search.js
+++ js/search.js
@@ -1,21 +1,31 @@
-const searchField = document.querySelector('.search-field');
+const searchField = document.querySelector(".search-field");
 const searchSedona = document.querySelector(".button-seach-sedona");
 const dateOn = searchField.querySelector("[name=date-on]");
+const dateOff = searchField.querySelector("[name=date-off]");
 const amountPeople = searchField.querySelector("[name=amount-of-people]");
 const amountChild = searchField.querySelector("[name=amount-of-child]");
 const form = searchField.querySelector("form");
 
-searchSedona.addEventListener("click", function() {
-  searchField.classList.toggle('active');
+searchSedona.addEventListener("click", function () {
+  searchField.classList.toggle("active");
+  searchField.classList.remove("error");
   dateOn.focus();
 });
 
-
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  localStorage.setItem("amountPeople", amountPeople.value);
-  localStorage.setItem("amountChild", amountChild.value);
-  console.log(amountPeople.value);
-  console.log(amountChild.value);
+  if (
+    !dateOn.value ||
+    !dateOff.value ||
+    !amountPeople.value ||
+    !amountChild.value
+  ) {
+    searchField.classList.add("error");
+  } else {
+    localStorage.setItem("amountPeople", amountPeople.value);
+    localStorage.setItem("amountChild", amountChild.value);
+    console.log(amountPeople.value);
+    console.log(amountChild.value);
+  }
+  setTimeout(() => searchField.classList.remove("error"), 1500);
 });
-
